75825192a9f727fdbea00d2f216d7a3904a8ef1f
vladickweb <vladickbaraza@gmail.com>
2022-10-08 09:49:38 +0200

    setup eslint prettier tailwind husky and antd

---
+++ pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Button } from 'antd'
+
+function HomePage() {
+	return (
+		<div>
+			<Button type='primary'>Hello World</Button>
+		</div>
+	)
+}
+
+export default HomePage
